b0499f8846bc828b1a9540e79817adbebb0f3b1b
Ludvig Lindblom <ludvig.lindblom@gmail.com>
2026-02-26 17:42:44 +0100

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<svelte:head>
+	<title>border-shape</title>
+</svelte:head>
+
+<h1>border-shape</h1>
+<p>border-shape is a powerful upcoming CSS primitive that defines a custom shape for an element's border.</p>
+<p>Una Kravets has written and excellent post about it at <a href="https://una.im/border-shape">https://una.im/border-shape</a>.</p>
+<p>You can also check out the <a href="https://drafts.csswg.org/css-borders-4/#border-shape">specification</a>.</p>
